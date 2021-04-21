ea490a326f2722041bb2e201145bfe9c25a14406
Mathew de Vin <mathewdevin03@gmail.com>
2021-04-20 21:41:32 -0700

    Animate text of landing page
    Animated each letter of the landing page name.
    
    Created a function that applies a timeline to each letter and waits for a hover event to occur. Then it plays the shrink animation to completion before it can repeat itself.

--- animations/animations.js
+++ animations/animations.js
@@ -12,3 +12,24 @@ export const spin = (target, {amount, duration, repeat}) => {
     transformOrigin: "50% 50%"
   });
 };
+
+export const eachShrink = (targets, amount) => {
+  const values = document.getElementsByClassName(targets);
+  for (let value of values) {
+    let playable = true;
+    let timeline = gsap.timeline({paused: true, onComplete: args => {playable = true}});
+    timeline.to(value, {
+      scale: amount,
+      duration: 0.15,
+      transformOrigin: "50% 50%"
+    }).to(value, {
+      scale: 1,
+      duration: 0.5,
+      transformOrigin: "50% 50%"
+    });
+    value.addEventListener('mouseover', () => {
+      playable ? timeline.restart() : null;
+      playable = false;
+    });
+  }
+};
