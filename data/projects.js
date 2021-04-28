2a66a4e4c8e4b6697c7ddbf0b21ac80d831ac933
Mathew de Vin <mathewdevin03@gmail.com>
2021-04-27 17:12:24 -0700

    Projects
    Added projects to the website. This includes images and data about the project.

--- data/projects.js
+++ data/projects.js
@@ -4,6 +4,54 @@ export const projects = [
     display: "constructed images",
     body:
       "A photography project that required the use of digital single-lens reflex photos alongside computer guided manipulation. Combining the use of architecture, paper, and digital structures to create unique constructed images.",
-    content: ["slide1.jpg", "slide2.jpg", "slide3.jpg", "slide4.jpg", "slide5.jpg"],
+    content: [
+      "slide1.jpg",
+      "slide2.jpg",
+      "slide3.jpg",
+      "slide4.jpg",
+      "slide5.jpg",
+    ],
+  },
+  {
+    name: "rock_paper_scissors",
+    display: "rock paper scissors",
+    body:
+      "A simple React.js application that explored the use of react hooks, effects, and props while replicating the rock paper scissors game. The use of css animations were involved all the while learning SCSS. The background is blurred and dynamic.",
+    content: [
+      "slide1.jpg",
+      "slide2.jpg",
+      "slide3.jpg"],
+  },
+  {
+    name: "exodus",
+    display: "exodus",
+    body:
+      "A platformer game, built using Java and the LibGDX framework. A personal project where you can play as a man trying to escape. Jump through dungeons and forests, killing skeletons and evil snails. The game consists of puzzles and boss battles at the end of each stage. Complete with custom music and art. Released on the Google Play Store, but no longer maintained.",
+    content: [
+      "slide1.jpg",
+      "slide2.jpg",
+      "slide3.jpg",
+      "slide4.jpg",
+      "slide5.jpg",
+      "slide6.jpg",
+      "slide7.jpg",
+    ],
+  },
+  {
+    name: "ecommerce",
+    display: "electric car ecommerce",
+    body:
+      "A project created as part of a university class. Required to build an ecommerce website within a 4 person team following specific guidelines. The group decided on creating an electric vehicle ecommerce shop where users can browse a wide selection of vehicles, add them to their carts, place orders, leave reviews, and if customers leave the shop their carts will be saved. Customers can log in and admins can view pages not accessible to customers. Stock can be managed and profits monitored.",
+    content: [
+      "slide1.jpg",
+      "slide2.jpg",
+      "slide3.jpg",
+      "slide4.jpg",
+      "slide5.jpg",
+      "slide6.jpg",
+      "slide7.jpg",
+      "slide8.jpg",
+      "slide9.jpg",
+    ],
   },
 ];
