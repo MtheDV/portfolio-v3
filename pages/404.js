d5942b13be4503cb12fdac97eafb68cb6687619a
Mathew de Vin <mathewdevin03@gmail.com>
2021-04-29 23:18:29 -0700

    Error 404 Page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// styles
+import customStyles from "../styles/404.module.scss";
+
+const Custom404 = () => {
+  return (
+    <section className={customStyles._404}>
+      <img src={"assets/images/sad.svg"} alt="sad face" />
+      <h3>Couldn't find what you were looking for</h3>
+    </section>
+  );
+};
+
+export default Custom404;
