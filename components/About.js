79197282677deeb52f039ad82f478221e986d8fe
Mathew de Vin <mathewdevin03@gmail.com>
2021-04-30 00:11:58 -0700

    Update About.js

--- components/About.js
+++ components/About.js
@@ -92,23 +92,28 @@ const About = ({ mouseX, mouseY }) => {
         onMouseLeave={() => setPreview(null, null)}
       >
         <p>
-          Web developer and designer. Currently attending the University of
-          British Columbia pursuing a Bachelors in Computer Science. Located in
-          the Okanagan.
+          Front-end creative web developer and designer. Currently attending the
+          University of British Columbia pursuing a Bachelors in Computer
+          Science. Located in the beautiful Okanagan, Canada.
         </p>
         <p>
           Although I’m constantly learning as a student, I’ve developed skills
-          through self-directed projects and collaborated projects using
-          JavaScript, HTML5, CSS3, React.js, Node.js, Express.js, SCSS/SASS,
-          GSAP. Redesigning and reorganizing a university team formation tool
-          allowing me to grow in Django and Python, while exploring the MVC
-          software design pattern. ExpressJS, SQL, noSQL. With skills also in
-          Java and Python development.
+          through self-directed projects and collaborated university projects
+          using JavaScript, HTML5, CSS3, React.js, Node.js, Express.js,
+          SCSS/SASS, GSAP, SQL, noSQL. This current website has allowed me to
+          learn and experience Next.js and the advantages that it provides. An
+          ongoing project of redesigning and reorganizing a university team
+          formation tool allowed me to grow in Django, Python, and Web
+          Development, while exploring the MVC software design pattern. I've
+          also gained skills in Java through small university projects and game
+          development.
         </p>
         <p>
-          Design skills developed using the Affinity Toolset (Photo, Designer,
-          and Publisher) and Figma alongside minor knowledge with the Adobe
-          Toolset (Photoshop and Illustrator).
+          I've developed my design skills using the Affinity Toolset (Photo,
+          Designer, and Publisher) and Figma alongside minimal use of the Adobe
+          Toolset (Photoshop and Illustrator). Co-leading a university design
+          club has allowed me to become vulnerable and grow while sharing my
+          ideas with others.
         </p>
       </div>
       <ImagePreview
